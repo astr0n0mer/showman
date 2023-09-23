8ce85755ec26b6df036a17acc217ff4a3fae9184
astr0n0mer <imran2000786@gmail.com>
2023-09-23 19:28:51 +0530

    Create store for managing movies data

---
+++ src/pages/api/_store.ts
@@ -0,0 +1,87 @@
+import type { Id } from "./_types";
+
+type CreateMovieDTO = {
+  title: string;
+  description: string;
+  releaseDate: Date;
+};
+
+export type Movie = {
+  id: Id;
+  title: string;
+  description: string;
+  releaseDate: Date;
+};
+
+export class MovieService {
+  private static service: MovieService;
+  private static idCounter = 0;
+  private movies: Movie[] = [];
+
+  private constructor() {}
+
+  public static getInstance = () => {
+    if (!MovieService.service) {
+      MovieService.service = new MovieService();
+    }
+    return MovieService.service;
+  };
+
+  public getOne = (id: Id) => {
+    const movie = this.movies.find((movie) => movie.id === id);
+    if (!movie) throw new Error("movie_does_not_exist");
+
+    return Promise.resolve(movie);
+  };
+
+  public getMany = () => Promise.resolve(this.movies);
+
+  public addOne = (movie: CreateMovieDTO): Promise<Id> => {
+    const id = ++MovieService.idCounter;
+    this.movies.push({ id, ...movie });
+    return Promise.resolve(id);
+  };
+
+  public deleteOne = (id: Id) => {
+    const oldLength = this.movies.length;
+
+    this.movies = this.movies.filter((movie) => movie.id !== id);
+    if (this.movies.length === oldLength)
+      throw new Error("movie_does_not_exist");
+
+    return Promise.resolve();
+  };
+
+  public updateOne = (id: Id, movie: Movie) => {
+    const oldLength = this.movies.length;
+
+    const newMovies = this.movies.filter((movie) => movie.id !== id);
+    if (newMovies.length === oldLength) throw new Error("movie_does_not_exist");
+
+    this.movies = [...newMovies, movie];
+    return Promise.resolve();
+  };
+}
+
+const movies: CreateMovieDTO[] = [
+  {
+    title: "Oppenheimer",
+    description:
+      "The story of American scientist, J. Robert Oppenheimer, and his role in the development of the atomic bomb.",
+    releaseDate: new Date("2023-09-1"),
+  },
+  {
+    title: "Tenet",
+    description:
+      "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
+    releaseDate: new Date("2022-10-15"),
+  },
+  {
+    title: "Dunkirk",
+    description:
+      "Allied soldiers from Belgium, the British Commonwealth and Empire, and France are surrounded by the German Army and evacuated during a fierce battle in World War II.",
+    releaseDate: new Date("2017-07-21"),
+  },
+];
+
+movies.forEach(async (movie) => await MovieService.getInstance().addOne(movie));
